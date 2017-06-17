1078d0d1aeb91ac347ea20685d7d2db7556e8bac
Wolfgang Wagner <wwagner9@bloomberg.net>
2017-06-17 14:07:38 -0400

    Add multiply feature to Math library

--- src/Math.cpp
+++ src/Math.cpp
@@ -8,4 +8,8 @@ double Math::add(const double a, const double b) {
   return (a+b);
 }
 
+double Math::multiply(const double a, const double b) {
+  return (a*b);
+}
+
 }  // close namespace DemoProject
--- tests/Math.t.cpp
+++ tests/Math.t.cpp
@@ -18,6 +18,27 @@ TEST(MathTest, addNeg) {
   EXPECT_LT(DemoProject::Math::add(-1,0), 0);
 }
 
+TEST(MathTest, mul0s) {
+  EXPECT_DOUBLE_EQ(DemoProject::Math::multiply(0,0), 0);
+}
+
+TEST(MathTest, mulTo0) {
+  EXPECT_DOUBLE_EQ(DemoProject::Math::multiply(1,0), 0);
+}
+
+TEST(MathTest, mulPos) {
+  EXPECT_GT(DemoProject::Math::multiply(1,1), 0);
+}
+
+TEST(MathTest, mulNeg) {
+  EXPECT_GT(DemoProject::Math::multiply(-1,-1), 0);
+}
+
+TEST(MathTest, mulNegPos) {
+  EXPECT_GT(DemoProject::Math::multiply(-1, 1), 0);
+}
+
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
